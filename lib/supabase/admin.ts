9f83354ff3517880a5770bd88416fe1aee0e237c
Your NameSRB <shapubgmobile@gmail.com>
2026-07-13 03:36:48 +0530

    Move Discord verification to secure API

--- lib/supabase/admin.ts
+++ lib/supabase/admin.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabaseAdmin = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL=https://lkkzikoiwybptorwdpox.supabase.co
-  process.env.SUPABASE_SERVICE_ROLE_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxra3ppa29pd3licHRvcndkcG94Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4MzYxNjMwNSwiZXhwIjoyMDk5MTkyMzA1fQ.wCOQ-JgT-Vsodc6AJi9T0fKySQiE59Rrxt9DILgHx6U
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_ROLE_KEY!,
   {
     auth: {
       autoRefreshToken: false,
