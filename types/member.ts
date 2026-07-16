8094164f05554c22e2bd0f6b326b27f49af0c887
Your NameSRB <shapubgmobile@gmail.com>
2026-07-17 01:34:26 +0530

    fix: restore Member type

---
+++ types/member.ts
@@ -0,0 +1,21 @@
+export interface Member {
+  id: string;
+
+  discord_id: string | null;
+
+  badge_number: string;
+  full_name: string;
+
+  rank: string;
+  department: string;
+
+  avatar: string | null;
+
+  joined_at: string | null;
+
+  duty_hours: number;
+  duty_days: number;
+
+  created_at?: string;
+  updated_at?: string;
+}
